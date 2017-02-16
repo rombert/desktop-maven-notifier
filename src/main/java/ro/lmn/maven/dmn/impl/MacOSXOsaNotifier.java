d75d742bb34409650e36a4dea303984613ec2931
Denis N. Antonioli <denis.antonioli@dematic.com>
2017-02-16 08:01:15 -0800

    Fix copyright

--- src/main/java/ro/lmn/maven/dmn/impl/MacOSXOsaNotifier.java
+++ src/main/java/ro/lmn/maven/dmn/impl/MacOSXOsaNotifier.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Radu Cotescu
+ * Copyright 2017 Denis N. Antonioli
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
